d5da7bcea17438adc98abe0486f319fec722e695
D'kie Rodriguez <drod702@yahoo.com>
2020-10-19 11:59:55 -0700

    change header title

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   render(){
     return (
       <div class="App">
-        <h1>ravenous</h1>
+        <h1>Dining Tonight?</h1>
         <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses = {this.state.businesses}/>
       </div>
